83bee19bcbe7bca044b2c5ee0466940c1ccbf649
徐文志程序猿 <azxuwen701@gmail.com>
2018-02-06 17:58:00 +0800

    Update init.el

--- init.el
+++ init.el
@@ -63,6 +63,29 @@
 ;;
 ;; ;; If you want to use last tagged version
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     ;; (package-installed-p 'evil)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+;; make sure to have downloaded archive description.
+;; Or use package-archive-contents as suggested by Nicolas Dudebout
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+(ensure-package-installed 'iedit 'magit) ;  --> (nil nil) if iedit and magit are already installed
+
+;; activate installed packages
 (package-initialize)
 
 
