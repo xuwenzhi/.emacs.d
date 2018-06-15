6999f54e634ad55ecf2a8dd38b3b243ad9340845
xuwenzhi <xuwenzhi@lianjia.com>
2018-06-15 20:23:36 +0800

    copy without cursor.

--- init.el
+++ init.el
@@ -120,7 +120,52 @@ Return a list of installed packages or nil for every skipped package."
 (setq interprogram-cut-function 'paste-to-osx) 
 (setq interprogram-paste-function 'copy-from-osx)
 
-(global-set-key (kbd "C-t") 'goto-line)
+(global-set-key (kbd "C-c t") 'goto-line)
+(global-set-key (kbd "C-c f")  'gtags-find-tag-other-window)
+
+;;copy without cursor
+(defun get-point (symbol &optional arg)
+      "get the point"
+      (funcall symbol arg)
+      (point)
+     )
+     
+     (defun copy-thing (begin-of-thing end-of-thing &optional arg)
+       "copy thing between beg & end into kill ring"
+        (save-excursion
+          (let ((beg (get-point begin-of-thing 1))
+         	 (end (get-point end-of-thing arg)))
+            (copy-region-as-kill beg end)))
+     )
+     
+     (defun paste-to-mark(&optional arg)
+       "Paste things to mark, or to the prompt in shell-mode"
+       (let ((pasteMe 
+     	 (lambda()
+     	   (if (string= "shell-mode" major-mode)
+     	     (progn (comint-next-prompt 25535) (yank))
+     	   (progn (goto-char (mark)) (yank) )))))
+     	(if arg
+     	    (if (= arg 1)
+     		nil
+     	      (funcall pasteMe))
+     	  (funcall pasteMe))
+     	))
+(defun copy-word (&optional arg)
+      "Copy words at point into kill-ring"
+       (interactive "P")
+       (copy-thing 'backward-word 'forward-word arg)
+       ;;(paste-to-mark arg)
+     )
+(global-set-key (kbd "C-c w") (quote copy-word))
+(defun copy-line (&optional arg)
+      "Save current line into Kill-Ring without mark the line "
+       (interactive "P")
+       (copy-thing 'beginning-of-line 'end-of-line arg)
+       (paste-to-mark arg)
+     )
+(global-set-key (kbd "C-c l") (quote copy-line))
+
 
 ;;php.
 (require 'php-mode)
